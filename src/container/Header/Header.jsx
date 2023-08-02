9a4e17d421c0136c3711cc6b5bbac6c7a2c50dbc
Gustavo Sepulveda <gustavosepulveda@ymail.com>
2023-08-02 15:43:53 -0500

    fixeda app__header scss

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,11 +58,13 @@ const Header = () => {
 				whileInView={scaleVariants.whileInView}
 				className="app__header-cirlces"
 			>
-				{[images.flutter, images.redux, images.sass].map((circle, index) => (
-					<div className="circle-cmp app__flex" key={`circle-${index}`}>
-						<img src={circle} alt="circle" />
-					</div>
-				))}
+				{[images.html, images.css, images.javascript, images.react, images.typescript, images.node].map(
+					(circle, index) => (
+						<div className="circle-cmp app__flex" key={`circle-${index}`}>
+							<img src={circle} alt="circle" />
+						</div>
+					)
+				)}
 			</motion.div>
 		</div>
 	)
